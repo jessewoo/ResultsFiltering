fd3cda6b9779ef4f8a0c8eeffd9502d0b7626629
Jesse Woo <woojesse@gmail.com>
2016-04-30 10:15:13 -0700

    Changes to IWA

--- public/js/iwa/form.js
+++ public/js/iwa/form.js
@@ -20,15 +20,36 @@ $("#questionsPagination ul li").on("click", function() {
     var question_id = $(this).attr("id");
     var trimmed_id = question_id.replace("-question","");
     // console.log(trimmed_id);
+
+    $("#WidgetContainer .form-group").addClass("hide");
+
+    // Pagination
     var selectedquestion_id = "#" + trimmed_id + "-question";
     $("#questionsPagination ul li").removeClass("active");
     $(selectedquestion_id).addClass("active");
 
     var loadhtmlfile = "iwa-files/" + trimmed_id + ".html";
 
-    $("#WidgetContainer").load(loadhtmlfile, function() {
-        console.log("Load " + trimmed_id + ".html");
-
-    });
+    // Do not AJAX load panels that have ALREADY been AJAX loaded
+    // This code checks to see if a section has previously been loaded or not
+    if ($('#WidgetContainer #' + trimmed_id + "-widgetcontainer").length > 0) {
+        // console.log("Previously loaded via AJAX: " + trimmed_id);
+        $('#WidgetContainer #' + trimmed_id + "-widgetcontainer").removeClass("hide");
+    }
+    // This panel don't exist, lazy load its content via AJAX
+    else {
+        // console.log('Downloading content for: ' + loadhtmlfile);
+        $.ajax({
+            url: loadhtmlfile,
+            dataType: "html",
+            error: function (jq, text, error) {
+                console.log('ERROR: ', jq, text, error);
+            },
+            success: function (data) {
+                // console.log('SUCCESS');
+                $("#WidgetContainer").append(data);
+            }
+        });
+    }
 
 })
